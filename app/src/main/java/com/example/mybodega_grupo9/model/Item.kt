73aaf299dfb5dd7e441f8ecda59270a517073212
CETECOM <CETECOM@AVAWSV01LC1024.duoc.acad>
2025-09-26 19:35:04 -0300

    feat: agregar modelo de datos Item con atributos básicos

---
+++ app/src/main/java/com/example/mybodega_grupo9/model/Item.kt
@@ -0,0 +1,9 @@
+package com.example.mybodega_grupo9.model
+
+data class Item(
+    val id: Int,
+    val nombre: String,
+    val categoria: String,
+    val cantidad: Int,
+    val ubicacion: String
+)
