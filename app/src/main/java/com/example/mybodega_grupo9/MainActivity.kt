72bc28388204df09ff308d6e5c261162104975d3
CETECOM <CETECOM@AVAWSV01LC1022.duoc.acad>
2025-09-30 20:15:49 -0300

    Actualizacion de pantalla principal como pide la guia (Pantalla HomeScreen con estructura Scaffold)

--- app/src/main/java/com/example/mybodega_grupo9/MainActivity.kt
+++ app/src/main/java/com/example/mybodega_grupo9/MainActivity.kt
@@ -3,45 +3,16 @@ package com.example.mybodega_grupo9
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.mybodega_grupo9.ui.theme.MyBodega_Grupo9Theme
+import com.example.mybodega_grupo9.ui.theme.HomeScreen
+
+
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
-            MyBodega_Grupo9Theme {
-                Scaffold( modifier = Modifier.fillMaxSize() ) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+            HomeScreen()  // 👈 Aquí llamas a tu pantalla principal
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    MyBodega_Grupo9Theme {
-        Greeting("Android")
     }
 }
