837c6a22be1da5a1bb255ae85874e93f649bd7b8
CETECOM <CETECOM@AVAWSV01LC1024.duoc.acad>
2025-10-03 19:44:42 -0300

    Navegación y menú generado

---
+++ app/src/main/java/com/example/mybodega_grupo9/navigation/NavGraph.kt
@@ -0,0 +1,35 @@
+package com.example.mybodega_grupo9.navigation
+
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.mybodega_grupo9.ui.theme.Screen.AdaptiveHome
+
+@Composable
+fun MyBodegaNavHost(startDestination: String = Screen.Home.route) {
+    val navController = rememberNavController()
+    NavHost(navController = navController, startDestination = startDestination) {
+        composable(Screen.Home.route) {
+            AdaptiveHome(onAdd = { navController.navigate(Screen.AddItem.route) })
+        }
+        composable(Screen.AddItem.route) {
+            AddItemScreen(onSaved = { navController.popBackStack() })
+        }
+        composable("details/{itemId}") { backStackEntry ->
+            val id = backStackEntry.arguments?.getString("itemId")?.toIntOrNull() ?: 0
+            DetailsScreen(itemId = id)
+        }
+    }
+}
+
+@Composable
+fun DetailsScreen(itemId: Int) {
+    TODO("Not yet implemented")
+}
+
+@Composable
+fun AddItemScreen(onSaved: () -> Boolean) {
+    TODO("Not yet implemented")
+}
